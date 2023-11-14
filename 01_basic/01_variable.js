c51d4c25d381c4c2dbb9cd5bdca6b7aa3690fe2e
MohdZain49 <144243648+MohdZain49@users.noreply.github.com>
2023-11-14 17:45:37 +0000

    learn variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,24 @@
+const accountId = 12234;
+let accountEmail = 'mohammedzain4099@gmail.com';
+var accountPassword = '123145';
+accountCity = 'Bareilly';
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
+// accountId = 2349; // not allowed 
+
+
+accountEmail = 'mohammadzain6398@gmail.com';
+// console.log(accountEmail);
+
+accountPassword = 232545;
+// console.log(accountPassword);
+
+accountCity = 'Aligarh';
+// console.log(accountCity);
+
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
