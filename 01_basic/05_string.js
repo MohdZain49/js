09547d8549b1710c0444fd29a02bb448fa7b16bc
MohdZain49 <144243648+MohdZain49@users.noreply.github.com>
2023-11-22 05:59:05 +0000

    learn string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,31 @@
+
+// Strings are used to manipulate and storing text data.
+
+
+let carName1 = 'Mahindra Thar';  // using single quote
+let carName2 = "Maruti Suzuki";  // using double quote
+
+console.log(carName1, carName2);
+
+
+let answer1 = "It's alright";       // using single quote in double quote
+let answer2 = "He is 'Jonny'";      // using single quote in double quote
+let answer3 = 'She is "Riya"';      // using double quote in single quote
+
+console.log(answer1);
+console.log(answer2);
+console.log(answer3);
+
+
+// Escape character or Escape sequence.
+
+// let text = "We are so-called "Vikings" from the north.";   // Not allowed.
+// console.log(text);
+
+let text1 = "We are so-called \"Vikings\" from north." 
+let text2 = 'It\'s alright.'
+let text3 = 'The character \\ is backslash.'
+
+console.log(text1);
+console.log(text2);
+console.log(text3);
