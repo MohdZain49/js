d1a4fd3015d178250a4671dcf07ba81b1d1dae8a
MohdZain49 <144243648+MohdZain49@users.noreply.github.com>
2023-12-04 07:52:18 +0000

    learns array

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,15 @@
+// ************** A R R A Y S ****************
+
+const cars = ['Saab', 'Volvo', 'BMW', 'Suzuki'];
+console.log(cars);
+
+// access array element by index
+let car = cars[2];
+console.log(car);
+
+// changing array element
+cars[3] = 'Toyoto';
+console.log(cars);
+
+// arrays are special type of objects
+console.log(`type of array is ${typeof cars}`);
